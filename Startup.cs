b7e0a183c632c113a15308c5e1eec889b4ecf387
shraddha <x19158742@student.ncirl.ie>
2020-10-04 16:19:52 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ENC.Startup))]
+namespace ENC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
